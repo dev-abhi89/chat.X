ca4584dd5982cc9d03799b7e845ab7911091c5d2
Abhishek Yadav <Abhishekydv2289@gmail.com>
2021-09-17 03:04:04 +0530

    completing proper chat functionality with realtime database

--- lib/home/components/body_chat.dart
+++ lib/home/components/body_chat.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutterchat/constrain.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BodyChat extends StatefulWidget {
   final String roomid;
@@ -27,12 +29,43 @@ class _BodyChatState extends State<BodyChat> {
               return ListView.builder(
                   itemCount: snapshot.data!.docs.length,
                   itemBuilder: (context, index) {
-                    return Container(child: Text(snapshot.data!.docs[index]['message']));
+                    Map dat = snapshot.data!.docs[index].data() as Map;
+                    return Messagebox(size: size, data: dat);
                   });
             } else {
               return Container();
             }
           }),
     );
+
+
   }
 }
+class Messagebox extends StatelessWidget {
+  const Messagebox({Key? key,required this.size, required this.data}) : super(key: key);
+ final Size size;
+ final  Map<dynamic ,dynamic> data;
+  @override
+  Widget build(BuildContext context){
+    bool send = ( data['sendby']==FirebaseAuth.instance.currentUser!.displayName);
+    return Row(
+      mainAxisAlignment: send? MainAxisAlignment.end:MainAxisAlignment.start
+      ,children:[ Flexible(
+        child: Container(
+          alignment:send? Alignment.centerRight:Alignment.centerLeft,
+          margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+          padding: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: send?kPrimaryLowColor:Color(0xFF62EA45)
+          ),
+          child: Text(data['message'],style: GoogleFonts.inter(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),),),
+      )]
+
+    );
+  }
+}
+
